6fcc74bb448585e6d3ec4b5f027b548a016a102e
Kristina Sviridova <KS.Kristina-2000@yandex.ru>
2025-10-26 17:31:29 +0300

    Добавлен метод, расчитывающий арифметические операции с числами

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,16 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        System.out.println("Введите первое число: ");
+        int number1 = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число: ");
+        int number2 = new Scanner(System.in).nextInt();
+        System.out.println("Сумма чисел = " + (number1 + number2));
+        System.out.println("Разность чисел = " + (number1 - number2));
+        System.out.println("Произведение чисел = " + (number1 * number2));
+        System.out.println("Частное чисел = " + ((double) number1 / number2));
     }
 }
