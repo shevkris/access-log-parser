3da8fbd5d13c5ae9d3039949521ad0ba0ff6e5d1
Kristina Sviridova <KS.Kristina-2000@yandex.ru>
2025-11-11 19:53:21 +0300

    Добавлен цикл, проверяющий указанный путь к файлу на корректность

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,30 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        int N = 0;
+        while (true) {
+            System.out.println("Введите путь к файлу: ");
+            String path = new Scanner(System.in).nextLine();
+
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+
+            if (!fileExists) {
+                System.out.println("Указанный файл не существует");
+                continue;
+            }
+            if (isDirectory) {
+                System.out.println("Указанный путь ведет к папке, а не к файлу");
+                continue;
+            }
+            System.out.println("Путь указан верно");
+            N++;
+            System.out.println("Путь №" + N);
+        }
     }
 }
