083a2a55cf8220a12a44cbfac037dd0e457ca6be
Kristina Sviridova <KS.Kristina-2000@yandex.ru>
2025-12-18 21:08:02 +0300

    чтение файла, подсчет самой длинной и короткой строки, исключения

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,19 @@
 package org.example;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.util.Scanner;
 
+
+class LineTooLongException extends RuntimeException {
+    public LineTooLongException(String message) {
+        super(message);
+    }
+}
+
 public class Main {
+    private static final int MAX_LINE_LENGTH = 1024;
     public static void main(String[] args) {
         int N = 0;
         while (true) {
@@ -25,6 +35,56 @@ public class Main {
             System.out.println("Путь указан верно");
             N++;
             System.out.println("Путь №" + N);
+            try {
+                readFileWithLengthCheck(path);
+            } catch (LineTooLongException e) {
+                System.err.println("ОШИБКА: " + e.getMessage());
+            }
+        }
+    }
+
+    private static void readFileWithLengthCheck(String filePath) {
+        try {
+            FileReader fileReader = new FileReader(filePath);
+            BufferedReader reader = new BufferedReader(fileReader);
+            String line;
+            int lineCounter = 0;
+            int maxLength = 0;
+            int minLength = Integer.MAX_VALUE;
+            while ((line = reader.readLine()) != null) {
+                lineCounter++;
+                int currentLength = line.length();
+                if (currentLength > MAX_LINE_LENGTH) {
+                    throw new LineTooLongException(
+                            String.format("Строка " + lineCounter + " слишком длинная: " + currentLength + " символов (максимум " + MAX_LINE_LENGTH + ")")
+                    );
+                }
+                if (currentLength > maxLength) {
+                    maxLength = currentLength;
+                }
+                if (currentLength > 0 && currentLength < minLength) {
+                    minLength = currentLength;
+                }
+            }
+            System.out.println("Всего строк: " + lineCounter);
+
+            if (lineCounter > 0) {
+                if (maxLength > 0) {
+                    System.out.println("Длина самой длинной строки: " + maxLength + " символов");
+
+                }
+                if (minLength != Integer.MAX_VALUE) {
+                    System.out.println("Длина самой короткой строки: " + minLength + " символов");
+                }
+            } else {
+                System.out.println("Файл пуст");
+            }
+
+        } catch (LineTooLongException e) {
+            throw e;
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
     }
 }
+
